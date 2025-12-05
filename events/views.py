bc36cf486360e9d2b08e56fdf6385b9b957fdb64
Siam Hossain <mdsiamh77@gmail.com>
2025-12-06 00:33:02 +0600

    added requerment

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect
 from events.models import Event, Category
 from django.utils import timezone
 from django.db.models import Q, Count,Sum
